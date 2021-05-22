59c32b7b49b902b3fc1097e159573086b62b2845
Gustavo William <gustavowilliam0980@gmail.com>
2021-05-22 15:10:33 -0300

    Adicionado exibição de perfis

--- perfis/views.py
+++ perfis/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 def index(request):
     return render(request, 'index.html')
+
+def exibir(request, id):
+    perfil = Perfil()
+    
+    if id == 1:
+        perfil = Perfil('Gustavo William', 'gustavo.oliveira1@estudante.ifb.edu.br', '222222', 'IFB')
+    if id == 2:
+        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla') 
+
+    return render(request, 'perfil.html', {'perfil': perfil})
+
