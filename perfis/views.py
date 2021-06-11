66e317317cdddcbc567b7e6b763e165205f616c6
Gustavo William <gustavowilliam0980@gmail.com>
2021-06-11 11:19:16 -0300

    Adicionado banco de dados

--- perfis/views.py
+++ perfis/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 from perfis.models import Perfil
+from perfis.models import Perfil
 
 def index(request):
     return render(request, 'index.html')
 
 def exibir(request, id):
-    perfil = Perfil()
-    
-    if id == 1:
-        perfil = Perfil('Gustavo William', 'gustavo.oliveira1@estudante.ifb.edu.br', '222222', 'IFB')
-    if id == 2:
-        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla') 
+    perfil = Perfil.objects.get(id=id)
 
     return render(request, 'perfil.html', {'perfil': perfil})
 
