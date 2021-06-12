52639c330a7bf26fc0e952110dae080aa6b9ac6f
Gustavo William <gustavowilliam0980@gmail.com>
2021-06-11 23:44:05 -0300

    Criado lista de perfis

--- perfis/views.py
+++ perfis/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 from perfis.models import Perfil
-from perfis.models import Perfil
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', { 'perfis' : Perfil.objects.all()})
 
 def exibir(request, id):
     perfil = Perfil.objects.get(id=id)
 
     return render(request, 'perfil.html', {'perfil': perfil})
-
