b1a6a04f8d8d89878b9269daa105de5e7bbe8f78
Gustavo William <gustavowilliam0980@gmail.com>
2021-07-04 10:37:56 -0300

    Adicionado funcionalidade de convidar

--- perfis/urls.py
+++ perfis/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('perfis/<int:id>', views.exibir, name='exibir'),
+    path('perfis/<int:id>/convidar', views.convidar, name='convidar'),
 ]
--- perfis/views.py
+++ perfis/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from perfis.models import Perfil
 
 def index(request):
@@ -8,3 +8,14 @@ def exibir(request, id):
     perfil = Perfil.objects.get(id=id)
 
     return render(request, 'perfil.html', {'perfil': perfil})
+
+def convidar(request, id):
+    perfil_a_convidar = Perfil.objects.get(id=id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+
+    return redirect('index')
+
+def get_perfil_logado(request):
+   return Perfil.objects.get(id=1)
+
